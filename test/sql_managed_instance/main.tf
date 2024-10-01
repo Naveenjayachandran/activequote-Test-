8df5bfd303e7b153145fe7181f4f4d5f0fb9f19f
Naveen Jayachandran <98024687+Naveenjayachandran@users.noreply.github.com>
2024-10-01 18:25:51 +0000

    Add files via upload

--- test/sql_managed_instance/main.tf
+++ test/sql_managed_instance/main.tf
@@ -1,1 +1,15 @@
+resource "azurerm_sql_managed_instance" "main" {
+  name                          = "sql-managed-instance"
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  administrator_login           = "sqladmin"
+  administrator_login_password   = "your_password_here"
+  subnet_id                     = azurerm_virtual_network.main.subnet[3].id
+  storage_size_in_gb           = 32
+  v_core                        = 4
+  version                       = "Gen5"
+}
 
+output "sql_managed_instance_name" {
+  value = azurerm_sql_managed_instance.main.name
+}
