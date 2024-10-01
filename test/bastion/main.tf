7c0159e53a07779b625da337b7ad6c3ed24552f5
Naveen Jayachandran <98024687+Naveenjayachandran@users.noreply.github.com>
2024-10-01 18:25:01 +0000

    Add files via upload

--- test/bastion/main.tf
+++ test/bastion/main.tf
@@ -1,1 +1,21 @@
+resource "azurerm_bastion_host" "main" {
+  name                = "bastion-host"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  ip_configuration {
+    name                 = "bastion-ip-configuration"
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
 
+resource "azurerm_public_ip" "bastion" {
+  name                = "bastion-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+  allocation_method   = "Static"
+}
+
+output "bastion_ip" {
+  value = azurerm_public_ip.bastion.ip_address
+}
